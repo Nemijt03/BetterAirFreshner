6c5add17882f80161007e95992a2d5b5f4f42622
Nemijt03 <Tijmenpvis@gmail.com>
2024-02-22 16:00:53 +0100

    menu.ino made

---
+++ menu.ino
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+
+int remainingShots = 2400; // maximum amount of shots
+
+byte degree[] = {
+  B01100,
+  B10010,
+  B10010,
+  B01100,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
+
+void toIdle() {
+    lcd.createChar(0, degree);
+    lcd.setCursor(0,0);
+    lcd.print(getTemperature());
+    lcd.write(byte(0));
+    lcd.print(F("C  S: "));
+    lcd.print(remainingShots);
+    lcd.setCursor(0,1);
+    lcd.print(F("Press "));
+    lcd.write(byte(B01111110));
+    lcd.print(F(" for menu"));
+    delay(2000);
+    lcd.noDisplay();
+}
+
+void updateIdle() {
+    lcd.home();
+    lcd.print(getTemperature());
+}
+
+void resetShots() {
+    remainingShots = 2400;
+    toIdle();
+}
+
+void toDelayConfig() {
+    
+}
+
+void toWhenToSpray() { // two choises
+
+}
